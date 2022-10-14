0ca73b09f5a4715408aa3f0bc0dc22bb4b0532eb
José A. Carrillo <josecarrillo1991.jc@gmail.com>
2022-10-14 14:44:01 -0500

    12-Eventos en JavaScript: interactuando con usuarios

--- js/main.js
+++ js/main.js
@@ -1,44 +1,13 @@
 
 const title = document.querySelector('h1');
-const p = document.querySelector('p');
-const p_class = document.querySelector('.p1');
-const p_id = document.querySelector('#p2');
-const input = document.querySelector('input');
-const cont = document.querySelector('#container');
+const input1 = document.querySelector('#num1');
+const input2 = document.querySelector('#num2');
 
-console.log(input.value);
+const btn = document.querySelector('#btnCalcular');
+const resul = document.querySelector('#suma')
+function btnClick() {
+    let suma = parseInt(input1.value) + parseInt(input2.value)
+    resul.append("La suma es: ",suma);
 
-// Muestra varios elementos
-console.log(title); // Muestra únicamente contenido
-
-console.log({
-    title,
-    p,
-    p_class,
-    p_id,
-    input,
-    cont
-})
-
-title.innerText = "Hola, este es mi nuevo titulo";
-console.log(title.getAttribute('class'));
-title.setAttribute('class', 'name');
-console.log(title.getAttribute('class'));
-
-title.classList.add('verde')
-title.classList.remove('name')
-// title.classList.toggle('name')
-// title.classList.contains('name')
-
-
-
-input.value = "Mi nuevo dato es:"
-
-
-// Crear elemento
-
-const img = document.createElement('img');
-img.setAttribute('alt','Mi imagen')
-img.setAttribute('src', 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif')
-
-cont.append(img)
+    console.log(num1+num2);
+}
